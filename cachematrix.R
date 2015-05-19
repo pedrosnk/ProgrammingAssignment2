eb33f1aa78cfc0f6bd930f56ac3acd2e459c059c
Pedro Medeiros <pedrosnk@gmail.com>
2015-05-19 20:37:10 -0300

    add some comments to the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the set of functions created to solve and cache the value of the inverse of a matrix
 
-## Write a short comment describing this function
+## This function is responsable to store a data structure that can be 
+## used to cache the resulve of the inverse of a matrix. 
+## The first and only argument must be a matrix.
+##
+## e.g. 
+##   makeCacheMatrix(matrix(1:4, 2))
 
 makeCacheMatrix <- function(x = matrix()) {
     cachedInverse <- NULL
@@ -24,7 +28,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function is responsable to solve a matrix that is stored at
+## the data structure created by the function makeCacheMatrix.
+## It accepts any argument that will be passed to the function solve
+## in order to calculate the cached matrix properlly or simply returns
+## its cached value.
 
 cacheSolve <- function(x, ...) {
     cachedValue <- x$getInverse()
